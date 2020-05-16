c1457e87ce11748edae8c24f4f6d91e867d5f56b
Rigel Kent Carbonel <rigel20.kent@gmail.com>
2020-05-16 20:45:11 +0800

    added sourceable

---
+++ src/Traits/Sourceable.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Luigel\TextToSpeech\Traits;
+
+trait Sourceable
+{
+    /**
+     * Determines where the text to convert from
+     *
+     * @var string
+     */
+    protected $source;
+
+    /**
+     * Sets the source
+     *
+     * @param string $source
+     * @return $this
+     */
+    public function source(string $source = 'text')
+    {
+        $this->source = $source;
+
+        return $this;
+    }
+
+    /**
+     * Get text from source
+     * @param mixed $data
+     * @return string
+     */
+    protected function getTextFromSource($data)
+    {
+        switch ($this->source)
+        {
+            case 'text':
+                return $data;
+            case 'path':
+                return $this->getTextFromPath($data);
+            case 'website':
+                return $this->getTextFromWebsite($data);
+
+        }
+    }
+
+    /**
+     * Get text from the path
+     *
+     * @param string $path
+     * @return string
+     */
+    protected function getTextFromPath(string $path)
+    {
+        return file_get_contents($path);
+    }
+
+    /**
+     * Get text from the website
+     *
+     * @param string $url
+     * @return string
+     */
+    protected function getTextFromWebsite(string $url)
+    {
+        // WIP
+    }
+}
